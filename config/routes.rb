6589b69aa5b6f4d7a827de2964e6a54fca00c076
msimon42 <msimon@uab.edu>
2020-07-11 17:21:57 -0600

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :buildings, only:[:index, :show] do
+    resources :offices, only:[:show]
+  end
+
+  resources :companies, only:[:show, :create, :destroy] do
+    resources :employees, only:[:show, :create, :destroy]
+  end
 end
