332efe03fd93a3a07400ae6ed11a1b5b4a760e08
msimon42 <msimon@uab.edu>
2020-07-11 13:06:03 -0600

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Office.destroy_all
+Employee.destroy_all
+Company.destroy_all
+Building.destroy_all
+
+
+buildings = FactoryBot.create_list(:building, 10)
+
+buildings.each do |building|
+  building.number_of_floors.times do |n|
+    FactoryBot.create(:office, building: building, floor: n)
+  end
+end
+
+offices = Office.all
+companies = FactoryBot.create_list(:company, (offices.length - 15))
+
+companies.each do |company|
+  office = Office.available.order('RAND()').first
+  company.offices << office
+
+  FactoryBot.create_list(:employee, rand(10..50), company: company)
+end
