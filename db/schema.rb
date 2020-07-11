606c3ee4f2388c9ba2f8f6bf594145551467efd2
msimon42 <msimon@uab.edu>
2020-07-10 20:55:26 -0600

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_11_024719) do
+ActiveRecord::Schema.define(version: 2020_07_11_025424) do
 
   create_table "buildings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -37,5 +37,14 @@ ActiveRecord::Schema.define(version: 2020_07_11_024719) do
     t.index ["company_id"], name: "index_employees_on_company_id"
   end
 
+  create_table "offices", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "building_id", null: false
+    t.string "floor"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["building_id"], name: "index_offices_on_building_id"
+  end
+
   add_foreign_key "employees", "companies"
+  add_foreign_key "offices", "buildings"
 end
