864380b524e00e03391bfe40eabedbea5eb4773c
msimon42 <msimon@uab.edu>
2020-07-10 21:25:06 -0600

    Add relationship tests

--- spec/models/office_spec.rb
+++ spec/models/office_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Office, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to(:company).optional }
+  end
 end
