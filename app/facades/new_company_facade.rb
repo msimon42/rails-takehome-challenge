2d92b1b08558beefc08cfeec32e7e58b8c6b0a06
msimon42 <msimon@uab.edu>
2020-07-12 13:32:04 -0600

    Add new company facade

---
+++ app/facades/new_company_facade.rb
@@ -0,0 +1,9 @@
+class NewCompanyFacade
+  def self.prepare_for_dropdown(offices)
+    dropdown_option = Struct.new(:id, :info)
+
+    offices.map do |office|
+      dropdown_option.new(office.id, "#{office.building.name}, Floor: #{office.floor}")
+    end
+  end
+end
