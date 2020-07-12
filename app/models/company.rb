9b887f61fbdcf0e8bff832c60b267bb8d2acfca1
msimon42 <msimon@uab.edu>
2020-07-12 13:32:47 -0600

    Add method that returns names of buildings being occupies

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,10 +1,15 @@
 class Company < ApplicationRecord
   has_many :offices
-  has_many :employees
+  has_many :buildings, through: :offices
+  has_many :employees, dependent: :destroy
 
   validates_presence_of :name
 
   def total_rent
     offices.sum(&:rent)
+  end
+
+  def building_names
+    buildings.pluck(:name)
   end
 end
