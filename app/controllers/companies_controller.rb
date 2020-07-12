a26753022a2b3db7eb04cc65c1fd85a9ea99f11a
msimon42 <msimon@uab.edu>
2020-07-12 13:31:31 -0600

    Add create and destroy actions

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,15 +1,39 @@
 class CompaniesController < ApplicationController
+  def index
+    @companies = Company.all
+  end
+
   def show
     @company = Company.find(params[:id])
   end
 
   def create
+    company = Company.create(company_params)
+    if company.save
+      office = Office.find(params[:office])
+      company.offices << office
+      flash[:success] = "#{company.name} successfully created!"
+      redirect_to company_path(company.id)
+    else
+      flash[:danger] = company.errors.full_messages.to_sentence
+      redirect_back fallback_location: new_company_path
+    end
+  end
 
+  def new
+    @available_offices = NewCompanyFacade.prepare_for_dropdown(Office.available)
+  end
+
+  def destroy
+    company = Company.find(params[:id])
+    company.destroy
+    flash[:success] = "#{company.name} removed from system."
+    redirect_to companies_path
   end
 
   private
 
   def company_params
-    params.require(:company).permit(:name)
+    params.permit(:name)
   end
 end
