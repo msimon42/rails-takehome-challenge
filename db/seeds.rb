2bb7a252a303576b60cf5676a5f6f628b898fe2b
msimon42 <msimon@uab.edu>
2020-07-12 19:05:43 -0600

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Office.destroy_all
 Employee.destroy_all
+Office.destroy_all
 Company.destroy_all
 Building.destroy_all
 
@@ -14,7 +14,7 @@ Building.destroy_all
 buildings = FactoryBot.create_list(:building, 10)
 
 buildings.each do |building|
-  building.number_of_floors.times do |n|
+  (1..building.number_of_floors).each do |n|
     FactoryBot.create(:office, building: building, floor: n)
   end
 end
@@ -26,5 +26,5 @@ companies.each do |company|
   office = Office.available.order('RAND()').first
   company.offices << office
 
-  FactoryBot.create_list(:employee, rand(10..50), company: company)
+  FactoryBot.create_list(:employee, rand(10..50), company: company, office: office)
 end
